e260c95a3eb1c2f84f9f6302ee52046ef334f7cb
big-bo99 <omuraliev.baurzhan@gmail.com>
2018-07-25 18:22:56 +0600

    updated serializers

--- api/serializers.py
+++ api/serializers.py
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 class TaskSerializer(serializers.ModelSerializer):
     assignee = UserSerializer(read_only=True)
-    # created_by = UserSerializer(required=False)
+    created_by = UserSerializer(required=False)
 
     class Meta:
         model = Task
